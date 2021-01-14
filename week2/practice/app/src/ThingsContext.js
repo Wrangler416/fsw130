ddf050a30aeba088ba722190d8db4b5b469f58a6
kara temple <you@example.com>
2021-01-14 11:14:11 -0400

    update

--- week2/practice/app/src/ThingsContext.js
+++ week2/practice/app/src/ThingsContext.js
@@ -24,29 +24,3 @@ export {ThingsContextProvider, Consumer as ThingsContextConsumer}
 
 
 
-
-
-// import React, {createContext, useState} from "react"
-
-// export const ThingsContext = createContext()
-
-
-// const ThingsContext = (props) => {
-
-//     const [things, setThing] = useState([
-
-//         { id: 1, 
-//           title: "Thing One", 
-//           description: "Ugliest Boat", 
-//           url: "https://www.yachtforums.com/attachments/really-ugly-boat-jpeg.84747/"
-//         }
-//     ]);
-  
-//     return (
-//        <ThingsContext.Provider value={things}>
-//            {props.children}
-//        </ThingsContext.Provider>
-//       )
-//     }
-    
-// export default ThingsContext
